edd3da8b930f88479d310e737d200afe23b411eb
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2024-12-21 15:41:46 +0000

    Update aes.py

--- src/symmetric/aes.py
+++ src/symmetric/aes.py
@@ -1,3 +1,13 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
+
+def encrypt(plaintext, key):
+    nonce = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CTR(nonce), backend=default_backend())
+    encryptor = cipher.encryptor()
+    return nonce + encryptor.update(plaintext) + encryptor.finalize()
+
+def decrypt(ciphertext, key):
+    decryptor = Cipher(algorithms.AES(key), modes.CTR(ciphertext[:16]), backend=default_backend()).decryptor()
+    return decryptor.update(ciphertext[16:]) + decryptor.finalize()
